b4855bd439b5d17a0e08711bf6b46594e186bdc5
h2so4v1 <84870291+h2so4v1@users.noreply.github.com>
2021-09-05 19:18:06 +0300

    Update ready.js

--- events/ready.js
+++ events/ready.js
@@ -1,6 +1,6 @@
 module.exports = async (client) => {
   console.log(`[API] Logged in as ${client.user.username}`);
-  await client.user.setActivity("!yardım -- Developer by Discord İd: gPx#8919 --gPxCode Music", { //Oynuyor Kısmı
+  await client.user.setActivity("!yardım -- Developer by gPx -- gPxCode Music", { //Oynuyor Kısmı
     type: "LISTENING",//LISTENING, WATCHING, PLAYING, STREAMING
   });
 };
